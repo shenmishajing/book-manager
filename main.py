1eead1ca3908de48a7980bafb27e2a0824bf8bb6
shenmishajing <shenmishajing@Gmail.com>
2019-04-15 00:44:23 +0800

    2019/04/15 00:44

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pymysql
 
+print('please input the passwd of root')
+passwd = input()
+
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd=passwd)
+
+
+def select_option(message, options, default=0):
+    pass
 
 
 print('please input the username')
@@ -12,18 +20,16 @@ db = pymysql.Connect(host='localhost', port=3306, user=user, passwd=passwd)
 
 cursor = db.cursor()
 
-
 try:
     cursor.execute("use library;")
-    cursor.execute("desc book;")
+    cursor.execute("desc borrow;")
     result = cursor.fetchall()
     for c in result:
+        print(c)
         print(c[0] + "," + c[1])
 
 except:
     print("database doesn't exist!")
-
-
 
 cursor.close()
 db.close()

4988ce5a0d6ff02df2b1de3dfbe12fea54a98acd
shenmishajing <shenmishajing@Gmail.com>
2019-04-16 20:51:34 +0800

    2019/04/16 20:51

--- execute_sql.py
+++ execute_sql.py
@@ -1,45 +1,50 @@
 import pymysql
 
 
-def execute_single_sql(db, cursor, sql):
-    try:
-        cursor.execute(sql)
-        db.commit()
-        result = cursor.fetchall()
-    except AttributeError as e:
-        print('The db var must be a pymysql.connect instance')
-        print('OR The version of your pymysql may not be right, you can get more information from requirements.txt')
-        print(e)
-        return "{}".format(e)
-    except ValueError as e:
-        print("The var sql must be valid sql query in string format or sequence format")
-        print(e)
-        return "{}".format(e)
-    except pymysql.err.IntegrityError as e:
-        print(e)
-        return "{}".format(e)
-    except pymysql.err.OperationalError as e:
-        print(e)
-        return '{}'.format(e)
-    except BaseException as e:
-        print(e)
-        return '{}'.format(e)
+def execute_single_sql(cursor, sql):
+    cursor.execute(sql)
+    result = cursor.fetchall()
 
     return result
 
 
 def execute_sql(db, sql):
-    cursor = db.cursor()
-    if isinstance(sql, list) or isinstance(sql, tuple):
-        result = []
-        for s in sql:
-            assert isinstance(s, str)
-            res = execute_single_sql(db, cursor, s)
-            result.append(res)
-    elif isinstance(sql, str):
-        result = execute_single_sql(db, cursor, sql)
-    else:
-        print('The var sql must be valid sql query in string format or sequence format')
-        return 0
+    try:
+        cursor = db.cursor()
+        if isinstance(sql, list) or isinstance(sql, tuple):
+            result = []
+            for s in sql:
+                assert isinstance(s, str)
+                res = execute_single_sql(cursor, s)
+                result.append(res)
+        elif isinstance(sql, str):
+            result = execute_single_sql(cursor, sql)
+        else:
+            print('The var sql must be valid sql query in string format or sequence format')
+            return 0
+        db.commit()
+    except AttributeError as e:
+        print('The db var must be a pymysql.connect instance')
+        print('OR The version of your pymysql may not be right, you can get more information from requirements.txt')
+        print(e)
+        db.rollback()
+        return "{}".format(e)
+    except ValueError as e:
+        print("The var sql must be valid sql query in string format or sequence format")
+        print(e)
+        db.rollback()
+        return "{}".format(e)
+    except pymysql.err.IntegrityError as e:
+        print(e)
+        db.rollback()
+        return "{}".format(e)
+    except pymysql.err.OperationalError as e:
+        print(e)
+        db.rollback()
+        return '{}'.format(e)
+    except BaseException as e:
+        print(e)
+        db.rollback()
+        return '{}'.format(e)
     cursor.close()
     return result
--- returnForm.py
+++ returnForm.py
@@ -6,7 +6,6 @@ from execute_sql import *
 class returnForm(insert.insertForm):
 
     def formOnShow(self, event):
-        self.control_list[2].Enable(False)
         self.control_list[4].Enable(False)
 
     def confirmButtonClick(self, event):
@@ -24,38 +23,39 @@ class returnForm(insert.insertForm):
             dlg.Destroy()
         else:
             stock = result[0][0]
-            if not stock:
-                dlg = wx.MessageDialog(None, u"书本没有库存", u"错误", wx.YES_NO | wx.ICON_QUESTION)
-                dlg.ShowModal()
-                dlg.Destroy()
+            old_date = self.control_list[2].GetValue()
+            old_date = "{}/{}/{}".format(old_date.year, old_date.month + 1, old_date.day)
+            sql = "select return_date from borrow where bno = '{}' and cno = '{}' and borrow_date = '{}';".format(
+                self.control_list[1].GetValue(), self.control_list[0].GetValue(), old_date)
+            result = execute_sql(self.db, sql)
+            if len(result) == 0:
+                temp = 1
             else:
-                sql = "select return_date from borrow where bno = '{}' and cno = '{}';".format(
-                    self.control_list[1].GetValue(), self.control_list[0].GetValue())
-                result = execute_sql(self.db, sql)
                 temp = result[0][0]
-                if temp is None:
-                    temp_date = self.control_list[3].GetValue()
-                    date = "{}/{}/{}".format(temp_date.year, temp_date.month + 1, temp_date.day)
+            if temp is None:
+                temp_date = self.control_list[3].GetValue()
+                date = "{}/{}/{}".format(temp_date.year, temp_date.month + 1, temp_date.day)
 
-                    sql = ['''
-                                    update borrow
-                                    set return_date='{}'
-                                    where bno='{}' and cno='{}';
-                                '''.format(date, self.control_list[1].GetValue(), self.control_list[0].GetValue()),
-                           '''
-                               update book
-                               set stock=stock+1
-                               where bno='{}';
-                               '''.format(self.control_list[1].GetValue())
-                           ]
-                    result = execute_sql(self.db, sql)
-                    if type(result) == str:
-                        dlg = wx.MessageDialog(None, u"信息更新失败", u"错误", wx.YES_NO | wx.ICON_QUESTION)
-                        dlg.ShowModal()
-                        dlg.Destroy()
-                    else:
-                        self.Close()
-                else:
-                    dlg = wx.MessageDialog(None, u"该用户已归还", u"错误", wx.YES_NO | wx.ICON_QUESTION)
+                sql = ['''
+                                update borrow
+                                set return_date='{}'
+                                where bno='{}' and cno='{}' and borrow_date='{}'
+                            '''.format(date, self.control_list[1].GetValue(), self.control_list[0].GetValue(),
+                                       old_date),
+                       '''
+                           update book
+                           set stock=stock+1
+                           where bno='{}';
+                           '''.format(self.control_list[1].GetValue())
+                       ]
+                result = execute_sql(self.db, sql)
+                if type(result) == str:
+                    dlg = wx.MessageDialog(None, u"信息更新失败", u"错误", wx.YES_NO | wx.ICON_QUESTION)
                     dlg.ShowModal()
                     dlg.Destroy()
+                else:
+                    self.Close()
+            else:
+                dlg = wx.MessageDialog(None, u"还书操作错误", u"错误", wx.YES_NO | wx.ICON_QUESTION)
+                dlg.ShowModal()
+                dlg.Destroy()
